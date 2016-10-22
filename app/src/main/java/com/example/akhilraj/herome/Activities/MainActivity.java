45d329e3b15c7b0683cfc2c62a47db27e9fb826c
Akhil Raj <akhilraj@Akhils-MacBook-Air.local>
2016-10-22 10:01:18 +0530

    added toolBar

---
+++ app/src/main/java/com/example/akhilraj/herome/Activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.akhilraj.herome.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.akhilraj.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
