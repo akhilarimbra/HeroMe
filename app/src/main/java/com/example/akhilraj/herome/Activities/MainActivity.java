283afd588f078dc8b8d4c2aae1b081f2aade96b4
Akhil Raj <akhilraj@Akhils-MacBook-Air.local>
2016-10-22 13:25:56 +0530

    integrated MainFragmentActivity to MainActivity

--- app/src/main/java/com/example/akhilraj/herome/Activities/MainActivity.java
+++ app/src/main/java/com/example/akhilraj/herome/Activities/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.akhilraj.herome.Activities;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.akhilraj.herome.Fragments.MainFragment;
 import com.example.akhilraj.herome.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
